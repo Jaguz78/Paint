e308f5f8d1f0c257aea0bb84dc201434fb54de08
jaguz <jalejandrogh78@gmail.com>
2021-05-23 19:06:09 -0600

    imagenes aleatoria funcionando pero podría ser mejor

---
+++ dibujandoConMouse/JS/challenge.js
@@ -0,0 +1,30 @@
+const principalColor = document.getElementById("principal-color")
+const lienzo = document.getElementById("dibujito");
+const papel = lienzo.getContext("2d");
+const liChallenge = document.getElementById("li-challenge");
+const challenge = document.getElementById("challenge");
+const iRandom = document.getElementById("iRandom")
+const imgChallenge = document.getElementById("challenge--img")
+const imgs = ["../img/cheems.png", "./img/gatoMotivado.jpg", "../img/satimaOK.jpg"]
+
+//----Llamada a funciones----
+liChallenge.addEventListener("click", displayOn);
+lienzo.addEventListener("click", displayOff);
+iRandom.addEventListener("click", changeImage);
+
+let intento = new Image();
+intento.src = "../img/gatoMotivado.jpg";
+intento.width = 10;
+intento.height = 10;
+
+/*----Funciones----*/
+function displayOn(){
+    challenge.classList.remove("hidden");
+}
+function displayOff(){
+    challenge.classList.add('hidden');
+}
+randomNumber = (max) => Math.floor(Math.random()*max);
+function changeImage(){
+    imgChallenge.src = imgs[Math.floor(randomNumber(imgs.length))];
+}
