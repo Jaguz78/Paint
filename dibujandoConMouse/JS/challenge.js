6787afd3861650323e8c90535a5229927ac6967b
jaguz <jalejandrogh78@gmail.com>
2021-05-27 14:15:45 -0600

    experimento tratando de arreglar el challenge

--- dibujandoConMouse/JS/challenge.js
+++ dibujandoConMouse/JS/challenge.js
@@ -3,10 +3,13 @@ const lienzo = document.getElementById("dibujito");
 const papel = lienzo.getContext("2d");
 const liChallenge = document.getElementById("li-challenge");
 const challenge = document.getElementById("challenge");
-const iRandom = document.getElementById("iRandom")
-const imgChallenge = document.getElementById("challenge--img")
-const imgs = ["../img/cheems.png", "./img/gatoMotivado.jpg", "../img/satimaOK.jpg", "../img/akatsuki.png", "../img/Pizzero.png",
-             "../img/react.jpg", "../img/red.jpg", "../img/obligame-prro.jpg"]
+const iRandom = document.getElementById("iRandom");
+const imgChallenge = document.getElementById("challenge--img");
+const imgs = ["https://image.freepik.com/vector-gratis/pollo-dibujos-animados_6996-1070.jpg",
+    "https://vignette.wikia.nocookie.net/club-penguin-super-fanon/images/1/11/Pizzero_3DS.png/revision/latest?cb=20160920005038&path-prefix=es",
+    "https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png",
+    "https://www.citypng.com/public/uploads/preview/-41603228248q6nqsvxdzn.png",
+    ]
 
 //----Llamada a funciones----
 liChallenge.addEventListener("click", displayOn);
